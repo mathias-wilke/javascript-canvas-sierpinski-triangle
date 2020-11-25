149551f72c7989a889e08bcfa63715b367e702db
mathias-wilke <mathias-wilke@users.noreply.github.com>
2020-11-25 22:35:34 +0100

    implemented basic functionality

--- js/canvas-fun.js
+++ js/canvas-fun.js
@@ -6,16 +6,91 @@ var ctx = canvas.getContext("2d");
 var backgroundColor = "#000000";
 
 //space for global variables. You may need some :)
-//@TODO: add some variables
+var width = document.getElementById("width").value;
+var recursion = document.getElementById("recursion").value;
 
 //implement your drawing here.
 function draw(){
-    //@TODO: implement your drawing here
+    ctx.fillStyle = "white";
+    ctx.font = "20px Arial";
+    ctx.fillText("JavaScript Canvas Sierpiński Triangle", 0, 15); 
+    ctx.font = "10px Arial";
+    ctx.fillText("Depth: " + recursion, 0, 40); 
+    ctx.fillText("Width: " + width, 0, 50); 
+    
+    ctx.translate((canvas.width / 2) - width / 2, canvas.height / 2);
+    var h = Math.tan(60 * (Math.PI / 180)) * width/2;
+    var x = Math.sqrt(Math.pow(width,2) - Math.pow(h,2));
+
+    ctx.beginPath();
+    ctx.moveTo(x,-h);
+    ctx.lineTo(-x,h);
+    ctx.lineTo((width*2)-x,h);
+    ctx.closePath();
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.stroke();
+
+    drawTriangle(recursion,width);
+}
+
+function drawTriangle(j,width){
+
+    if(j <= 0){
+        return;
+    }
+
+    var h = Math.sqrt(3) * width / 2;
+    var x = (h/2) / Math.tan(60 * (Math.PI / 180));
+
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(width,0);
+    ctx.lineTo(width/2,h);
+    ctx.closePath();
+
+    var colored = document.getElementById("colors").value;
+    var filled = document.getElementById("fill").value;
+
+    if(colored == 1){
+        var gradient = ctx.createLinearGradient(0, 0, 800, 800);
+        gradient.addColorStop("0", rainbow(recursion,j));
+        gradient.addColorStop("1", rainbow(recursion,j));
+        ctx.strokeStyle = gradient;
+        ctx.fillStyle = gradient;
+    }else{
+        ctx.strokeStyle = "#FFFFFF";
+        ctx.fillStyle = "#FFFFFF";
+    }
+
+    if(filled == 1){
+        ctx.fill();
+    }
+
+    ctx.stroke();
+
+    //translate 1
+    ctx.save();
+    ctx.translate((-width/2) + x,h/2);
+    drawTriangle(j - 1, width / 2);
+    ctx.restore();
+
+    //translate 2
+    ctx.save();
+    ctx.translate((width/2) + x,h/2);
+    drawTriangle(j - 1, width / 2);
+    ctx.restore();
+
+    //translate 2
+    ctx.save();
+    ctx.translate((width/2)-x,-h/2);
+    drawTriangle(j - 1, width / 2);
+    ctx.restore();
 }
 
 //calculate, sort, or do whatever you want here
 function update(){
-    //@TODO: implement your calculations here
+    width = document.getElementById("width").value;
+    recursion = document.getElementById("recursion").value;
 }
 
 //clear the canvas
