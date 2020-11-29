ab8324fd4b10cc0fa3105172f32fb6cdc1c4e02a
mathias-wilke <mathias-wilke@users.noreply.github.com>
2020-11-29 13:09:13 +0100

    updated UI to use the full width

--- js/canvas-fun.js
+++ js/canvas-fun.js
@@ -13,11 +13,11 @@ var recursion = document.getElementById("recursion").value;
 function draw(){
     ctx.fillStyle = "white";
     ctx.font = "20px Arial";
-    ctx.fillText("JavaScript Canvas Sierpiński Triangle", 0, 15); 
+    ctx.fillText("JavaScript Canvas Sierpiński Triangle", 5, canvas.height - 60); 
     ctx.font = "10px Arial";
-    ctx.fillText("Depth: " + recursion, 0, 40); 
-    ctx.fillText("Width: " + width, 0, 50); 
-    ctx.fillText("https://github.com/mathias-wilke/javascript-canvas-sierpinski-triangle.git", 0, canvas.height - 4); 
+    ctx.fillText("Depth: " + recursion, 5, canvas.height - 50); 
+    ctx.fillText("Width: " + width, 5, canvas.height - 40); 
+    ctx.fillText("https://github.com/mathias-wilke/javascript-canvas-sierpinski-triangle.git", 5, canvas.height - 10); 
 
     ctx.translate((canvas.width / 2) - width / 2, canvas.height / 2);
     var h = Math.tan(60 * (Math.PI / 180)) * width/2;
@@ -103,8 +103,8 @@ function clear(){
 
 //if the user changes the size of the window we have do recalculate
 function resizeCanvas(){
-    canvas.width = window.innerWidth - 20;
-    canvas.height = window.innerHeight -20;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
 
 //let us call the function once at the start to get the user's canvas size
